b10d5833ab42bccb7b3c40cafd4b3a9c5f85cfc2
Abdullah Khalid <engabdullah.khaled10@gmail.com>
2023-10-01 20:23:02 +0300

    Version 1.1

--- Code/PCServer.cpp
+++ Code/PCServer.cpp
@@ -7,73 +7,87 @@
 
 int main(int argc, char const *argv[])
 {
-        int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-        struct sockaddr_in server_address;
+    // Create a server socket
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_address;
 
-        server_address.sin_family = AF_INET;
-        server_address.sin_port = htons(8080);
-        server_address.sin_addr.s_addr = INADDR_ANY;
+    // Set up the server's address information
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
-        
-        bind(server_socket,(struct sockaddr*)&server_address,sizeof(server_address));
+    // Bind the server socket to the specified address and port
+    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
 
-        listen(server_socket,5);
+    // Start listening for incoming connections
+    listen(server_socket, 5);
 
-        std::cout << "Server is listening on port: 8080" << std::endl;
+    std::cout << "Server is listening on port: 8080" << std::endl;
 
-        char buffer[1024]= "Choose Command to be Processed: \n 01- Open Firefox \n 02- Open Terminal \n 03- Open Nautilus";
+    // Message to be sent to the client
+    char buffer[1024] = "Choose Command to be Processed: \n 01- Open Firefox \n 02- Open Terminal \n 03- Open Nautilus";
 
-        int client = accept(server_socket,NULL,NULL);
+    // Accept a client connection
+    int client = accept(server_socket, NULL, NULL);
 
-        int bytes = 1;
+    // Number of bytes read from the client
+    int bytes = 1;
 
-        send(client, buffer, sizeof(buffer), 0);
+    // Send the buffer message to the client
+    send(client, buffer, sizeof(buffer), 0);
 
-    while(bytes)
+    // Loop to receive and process commands from the client
+    while (bytes)
     {
-        memset(buffer,0,sizeof(buffer));
+        // Clear the buffer
+        memset(buffer, 0, sizeof(buffer));
 
-        bytes = read(client,buffer,sizeof(buffer));
+        // Read data from the client into the buffer
+        bytes = read(client, buffer, sizeof(buffer));
 
-        std::cout << "Recieved: " << buffer << std::endl;
+        // Print the received data
+        std::cout << "Received: " << buffer << std::endl;
 
+        // Fork a new process
         pid_t pid = fork();
-        if((strcmp(buffer,"terminal\n")==0))
+        if ((strcmp(buffer, "terminal\n") == 0))
         {
-            std::cout << "ter" << std::endl;
-            if(pid == 0)
+            // Execute code for opening terminal
+            if (pid == 0)
             {
-                execlp("gnome-terminal" , "gnome-terminal" , NULL);
+                execlp("gnome-terminal", "gnome-terminal", NULL);
                 exit(0);
             }
         }
-        else if ((strcmp(buffer,"browser\n")==0))
+        else if ((strcmp(buffer, "browser\n") == 0))
         {
-            std::cout << "fire" << std::endl;
-            if(pid == 0)
+            // Execute code for opening browser
+            if (pid == 0)
             {
-                execlp("firefox" , "firefox" , NULL);
+                execlp("firefox", "firefox", NULL);
                 exit(0);
             }
         }
-        else if ((strcmp(buffer,"nautilus\n")==0))
+        else if ((strcmp(buffer, "nautilus\n") == 0))
         {
-            std::cout << "nauti" << std::endl;
-            if(pid ==0)
+            // Execute code for opening nautilus
+            if (pid == 0)
             {
-                execlp("nautilus" , "nautilus" , NULL);
+                execlp("nautilus", "nautilus", NULL);
                 exit(0);
             }
         }
         else
         {
-            memset(buffer,0,sizeof(buffer));
-            strcpy(buffer,"Wrong Command");
-            std::cout << "wrong" << std::endl;
+            // Execute code for wrong command
+            memset(buffer, 0, sizeof(buffer));
+            strcpy(buffer, "Wrong Command");
+            std::cout << "Wrong command" << std::endl;
             send(client, buffer, sizeof(buffer), 0);
         }
     }
 
+    // Check if the client disconnected or an error occurred
     if (bytes == 0)
     {
         std::cout << "Client Disconnected" << std::endl;
@@ -82,8 +96,10 @@ int main(int argc, char const *argv[])
     {
         std::cout << "Error!!" << std::endl;
     }
-    
-    close (server_socket);
+
+    // Close the server and client sockets
+    close(server_socket);
     close(client);
+
     return 0;
 }
